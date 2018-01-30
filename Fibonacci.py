a2f7a89ad3d0f707ccb40f57ffec4ae13f28859e
Huey Kazahaya <annosatori@gmail.com>
2018-01-30 16:31:49 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -7,27 +7,28 @@
 # r = 10**9 + 7
 
 import sys
-import math
 
 global r
 r = 10**9 + 7
 
-def multi(A, B):
-    return([
-        [(A[0][0] % r) * (B[0][0] % r) + (A[0][1] % r) * (B[1][0] % r), 
-        (A[0][0] % r) * (B[0][1] % r) + (A[0][1] % r) * (B[1][1] % r)], 
-        [(A[1][0] % r) * (B[0][0] % r) + (A[1][1] % r) * (B[1][0] % r),
-        (A[1][0] % r) * (B[0][1] % r) + (A[1][1] % r) * (B[1][1] % r)]
-        ])
+def matmultiply(A, B):
+    if len(A[0]) != len(B):
+        return False
+    C = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
 
 def power(A, n):
     if n == 1:
         return A
     elif n % 2:
-        return multi(A, power(A, n - 1))
+        return matmultiply(A, power(A, n - 1))
     else:
         B = power(A, n // 2)
-        return multi(B, B)
+        return matmultiply(B, B)
 
 def fib(n):
     M = [[1,1],[1,0]]
@@ -39,8 +40,7 @@ def fib(n):
         M = power(M, n - 1)
         return M[0][0] % r
 
-
-''' Main '''
+# Main
 
 if __name__ == '__main__':
     for _ in range(int(input())):
